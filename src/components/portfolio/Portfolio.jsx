5bf0099d789539e5bbba2d9ed0017f766382ac78
Ashwin Singh <rajputashwin87@gmail.com>
2022-08-13 01:48:22 +0530

    xxx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assests/portfolio1.jpg'
+import IMG2 from '../../assests/portfolio2.jpg'
+import IMG3 from '../../assests/portfolio3.jpg'
+import IMG4 from '../../assests/portfolio4.jpg'
+import IMG5 from '../../assests/portfolio5.png'
+import IMG6 from '../../assests/portfolio6.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard',
+    github: 'https://github.com',
+    demo: '_blank'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Table Maker',
+    github: 'https://github.com',
+    demo: '_blank'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Search Engine Optimization',
+    github: 'https://github.com',
+    demo: '_blank'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Calculator',
+    github: 'https://github.com',
+    demo: '_blank'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Chess Game',
+    github: 'https://github.com',
+    demo: '_blank'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Puzzle Game',
+    github: 'https://github.com',
+    demo: '_blank'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id ='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo})=>{
+            return(
+        <article key={id} className='portfolio__item'>
+          <div className="portfolio__item-image">
+          <img src={image} alt={title} />
+          </div>
+            <h3>{title}</h3>
+           <div className="portfolio__item-cta">
+           <a href={github} className='btn' target= '_blank'>Github</a>
+            <a href={demo} className='btn btn-primary'target= '_blank'>Live Demo</a>
+           </div>
+         
+        </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
