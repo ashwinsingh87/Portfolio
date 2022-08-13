186c0429a9703d609306186fdc4d0abdf525c6b6
Ashwin Singh <rajputashwin87@gmail.com>
2022-08-13 12:37:55 +0530

    xxx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,8 +7,8 @@ import {IoLogoTwitter} from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-      <a href = "#" className ='footer__logo'>EGATOR</a>
-      <ul className='permalinks'>
+      {/* <a href = "#" className ='footer__logo'>EGATOR</a> */}
+      {/* <ul className='permalinks'>
         <li><a href='#' >Home</a></li>
         <li><a href='#about' >About</a></li>
         <li><a href='#experience' >Experience</a></li>
@@ -16,7 +16,7 @@ const Footer = () => {
         <li><a href='#portfolio' >Portfolio</a></li>
         <li><a href='#testimonials' >Testimonials</a></li>
         <li><a href='#contact' >Contact</a></li>
-      </ul>
+      </ul> */}
 
       <div className="footer__socials">
         <a href="https://facebook.com"><FaFacebookF/></a>
