9f1f55fceb5a94c1d3acc8965b958703bbfb878d
Ashwin Singh <91330777+ashwinsingh87@users.noreply.github.com>
2023-05-11 23:08:29 +0530

    commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,6 +9,35 @@ import IMG6 from '../../assests/portfolio6.jpg'
 
 const data = [
   {
+    id: 6,
+    image: IMG3,
+    title: 'Amazon Clone',
+    github: 'https://github.com/ashwinsingh87/Amazon-clone.git',
+    demo: false
+  },
+
+  {
+    id: 3,
+    image: IMG2,
+    title: 'IMDB-Clone',
+    github: 'https://github.com/ashwinsingh87/imdb-clone',
+    demo: 'https://imdb-ashwin.netlify.app/'
+  },
+  {
+    id: 4,
+    image: IMG1,
+    title: 'Video Application',
+    github: 'https://github.com/ashwinsingh87/youtube-app.git',
+    demo: 'https://ytvideoapp.netlify.app/'
+  },
+  {
+    id: 5,
+    image: IMG6,
+    title: 'Food App',
+    github: 'https://github.com/ashwinsingh87/FoodApp.git',
+    demo: false
+  },
+  {
     id: 1,
     image: IMG4,
     title: 'Tesla-Clone',
@@ -21,58 +50,31 @@ const data = [
     title: 'Portfolio Website',
     github: 'https://github.com/ashwinsingh87/Portfolio',
     demo: 'https://ashwinsingh87.github.io/Portfolio/'
-  }, 
-  {
-    id: 3,
-    image: IMG2,
-    title: 'IMDB-Clone',
-    github: 'https://github.com/ashwinsingh87/imdb-clone',
-    demo: 'https://imdb-ashwin.netlify.app/'
   },
-  {
-    id: 4,
-    image: IMG1,
-    title: 'Vehicle Detection',
-    github: 'https://github.com/ashwinsingh87/VehicleDetection',
-    demo: 'https://github.com/ashwinsingh87/VehicleDetection'
-  },
-  {
-    id: 5,
-    image: IMG6,
-    title: 'File Organizer',
-    github: 'https://github.com/ashwinsingh87/file-organizer',
-    demo: 'https://github.com/ashwinsingh87/file-organizer'
-  },
-  {
-    id: 6,
-    image: IMG3,
-    title: 'Brick-Breaker-Game',
-    github: 'https://github.com/ashwinsingh87/Brick-Breaker-',
-    demo: 'https://github.com/ashwinsingh87/Brick-Breaker-'
-  }
-  
+
+
 ]
 
 const Portfolio = () => {
   return (
-    <section id ='portfolio'>
+    <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
         {
-          data.map(({id, image, title, github, demo})=>{
-            return(
-        <article key={id} className='portfolio__item'>
-          <div className="portfolio__item-image">
-          <img src={image} alt={title} />
-          </div>
-            <h3>{title}</h3>
-           <div className="portfolio__item-cta">
-           <a href={github} className='btn' target= '_blank' rel="noreferrer">Github</a>
-            <a href={demo} className='btn btn-primary'target= '_blank' rel="noreferrer">Live Demo</a>
-           </div>
-         
-        </article>
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank' rel="noreferrer">Github</a>
+                  {demo ? <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a> : null}
+                </div>
+
+              </article>
             )
           })
         }
